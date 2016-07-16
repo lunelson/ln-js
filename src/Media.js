857c674fd5abf69e43ded9949a24ebc78dbe0262
Lu Nelson <lunelson@gmail.com>
2016-07-16 19:27:48 +0200

    start new media.js

--- src/Media.js
+++ src/Media.js
@@ -11,13 +11,31 @@ const cssMedia = require('./css-data').media;
 const Media = Object.assign(new Emitter(), {
 
   init() {
-    if (this.initialized) throw new Error('Pjax: attempted to initialize twice');
-    this.cssMedia = cssMedia;
 
-    console.log(this.cssMedia);
-    Object.keys(this.cssMedia).forEach((mKey)=>{
-      console.log(this.cssMedia[mKey].breakpoint);
-    })
+    // init check
+    if (this.initialized) throw new Error('Pjax: attempted to initialize twice');
+
+    // build breakPoints object
+    const breakPoints = mediaKeys.reduce((obj, key) => {
+      obj[key] = cssMedia[key][breakpoint];
+      return obj;
+    },{});
+
+    // build breakPointEvents object
+    const breakPointEvents = mediaKeys.reduce((obj, key) => {
+      obj[key] = global.matchMedia(`(min-width: ${breakPoints[key]})`);
+      return obj;
+    },{});
+
+    // build breakPointHandlers object
+    const breakPointHandlers = mediaKeys.reduce((obj, key, i, mediaKeys) => {
+      obj[key] = (query) => {
+        this.trigger('change');
+        this.trigger(`enter-${query.matches ? mediaKeys[i + 1] : key}`);
+        this.trigger(`exit-${query.matches ? key : mediaKeys[i + 1]}`);
+      };
+      return obj;
+    },{});
 
     this.initialized = true;
     return this;
