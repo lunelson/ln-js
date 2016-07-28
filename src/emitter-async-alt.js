246dfd0cb1b363c039e51fa2c0c67835ebd7c8b4
Lu Nelson <lunelson@gmail.com>
2016-07-28 10:56:16 +0200

    tinker with async emitter

--- src/emitter-async-alt.js
+++ src/emitter-async-alt.js
@@ -11,43 +11,49 @@ require('setimmediate');
 
 function applyAndReset(fn, args) { fn.apply(null, args); fn._pending = false; }
 
+function requestify(fn, cancellable) {
+  function cb(...args){
+    fn.apply(null, args);
+    cb._pending = null;
+  }
+  cb._pending = null;
+  cb._cancellable = cancellable;
+  return cb;
+}
+
 class AsyncEmitter {
 
   constructor() { this.fnSets = {}; }
 
   on(event, measure, mutate, cancellable = [false, false]) {
+
     let fnSet = this.fnSets[event] = this.fnSets[event] || {};
     let measureFns = fnSet.measure = fnSet.measure || [];
     let mutateFns = fnSet.mutate = fnSet.mutate || [];
 
-    if (measure) {
-      measure._pending = false;
-      measure._cancellable = cancellable[0];
-    }
-    if (mutate) {
-      mutate._pending = false;
-      mutate._cancellable = cancellable[1];
-    }
+    measure && (measure = requestify(measure, cancellable[0]));
+    mutate && (mutate = requestify(mutate, cancellable[1]));
 
-    measureFns.push();
-    mutateFns.push();
+    measureFns.push(measure);
+    mutateFns.push(mutate);
 
     return measureFns.length - 1;
   }
 
   trigger(event, ...args) {
+
     let fnSet = this.fnSets[event];
     let measureFns = fnSet.measure;
     let mutateFns = fnSet.mutate;
 
     measureFns && measureFns.forEach((measure) => {
       if (measure && measure._pending && measure._cancellable) { clearImmediate(measure._pending); }
-      if (measure && !measure._pending) { measure._pending = setImmediate(applyAndReset.bind(null, measure, args)); }
+      if (measure && !measure._pending) { measure._pending = setImmediate(measure); }
     });
 
     mutateFns && mutateFns.forEach((mutate) => {
       if (mutate && mutate._pending && mutate._cancellable) { cancelAnimationFrame(mutate._pending); }
-      if (mutate && !mutate._pending) { mutate._pending = requestAnimationFrame(applyAndReset.bind(null, mutate, args)); }
+      if (mutate && !mutate._pending) { mutate._pending = requestAnimationFrame(mutate); }
     });
   }
 }
